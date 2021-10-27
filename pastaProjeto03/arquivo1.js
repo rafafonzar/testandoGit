919dafee427a871f4e2d74658a7c8a67c3668075
rafafonzar <rafaelfonzaralb@gmail.com>
2021-10-27 14:47:47 -0300

    New function. Subtraction

--- pastaProjeto03/arquivo1.js
+++ pastaProjeto03/arquivo1.js
@@ -2,4 +2,11 @@ function imprimirSoma(a, b) {
     console.log(a + b)
 }
 
+function imprimirSubtracao(a, b) {
+    console.log(a - b)
+}
+
+
+console.log("A soma dos numeros eh:")
 imprimirSoma(2, 3)
+imprimirSubtracao(10, 4)
